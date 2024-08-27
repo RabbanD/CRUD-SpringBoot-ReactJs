141b6d6a993be4be58484ee5b6fe8e24fcbfe52d
RabbanD <rabbandange1234@gmail.com>
2024-08-27 15:53:16 +0530

    localhost changes

--- Springboot React CRUD/src/Pages/Employee/PostUser.jsx
+++ Springboot React CRUD/src/Pages/Employee/PostUser.jsx
@@ -29,7 +29,7 @@ const PostUser = () => {
         console.log(formData)
 
         try {
-            const response = await fetch("http://localhost:8080/api/employee",{
+            const response = await fetch("http://mysql.railway.internal:8080/api/employee",{
                 method: "POST",
                 headers: {"Content-Type":"application/json"},
                 body: JSON.stringify(formData)
