9b861dfe147e83d41116a59075dff08afc277c97
RabbanD <rabbandange1234@gmail.com>
2024-08-27 16:39:21 +0530

    again corrected APIS

--- Springboot React CRUD/src/Pages/Dashboard/Dashboard.jsx
+++ Springboot React CRUD/src/Pages/Dashboard/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
     useEffect(()=>{
         const fetchEmployees = async () => {
             try {
-                const response = await fetch("http://spring-backend-production-dd4b.up.railway.app:8080/api/employees");
+                const response = await fetch("http://localhost:8080/api/employees");
                 const data = await response.json();
 
                 setEmployees(data);
@@ -29,7 +29,7 @@ const Dashboard = () => {
 
     const handleDelete = async(employeeId) =>{
         try {
-            const response = await fetch(`http://spring-backend-production-dd4b.up.railway.app:8080/api/employee/${employeeId}`,{
+            const response = await fetch(`http://localhost:8080/api/employee/${employeeId}`,{
                 method: "DELETE",
             });
 
--- Springboot React CRUD/src/Pages/Employee/UpdateUser.jsx
+++ Springboot React CRUD/src/Pages/Employee/UpdateUser.jsx
@@ -27,7 +27,7 @@ const UpdateUser = () => {
     useEffect(()=>{
         const fetchEmployee = async ()=>{
             try {
-                const response = await fetch(`http://spring-backend-production-dd4b.up.railway.app:8080/api/employee/${id}`);
+                const response = await fetch(`http://localhost:8080/api/employee/${id}`);
                 const data = await response.json();
                 setFormData(data);
             } catch (error) {
@@ -41,7 +41,7 @@ const UpdateUser = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch(`http://spring-backend-production-dd4b.up.railway.app:8080/api/employee/${id}`,{
+            const response = await fetch(`http://localhost:8080/api/employee/${id}`,{
                 method:"PATCH",
                 headers:{
                     "Content-Type":"application/json",
